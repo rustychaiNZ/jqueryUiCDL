91188af758298a40bfbfabc92b6f9c4590023fbd
Russell grant Johnson <932400859@bun2-206-14.local>
2019-08-30 13:47:15 +1200

    fixed on click btn function

--- js/script.js
+++ js/script.js
@@ -3,63 +3,84 @@ console.log('My script is working')
 $( function() {
 
 	// Menu with drop down options
-    $("#menu").menu();
+	$("#menu").menu();
 
-    // Auto Complete tags
-    var availableTags = [
-    "Minecraft", "Grand Theft Auto", "Prey", "Teraria", "Forza", "Fable", "Left 4 Dead",
-    ];
+	// Auto Complete tags
+	var availableTags = [
+	"Minecraft", "Grand Theft Auto", "Prey", "Teraria", "Forza", "Fable", "Left 4 Dead",
+	];
 
-    // Auto Complete input
-    $("#tags").autocomplete({
-    	source: availableTags
-    });
+	// Auto Complete input
+	$("#tags").autocomplete({
+		source: availableTags
+	});
 
-    // Accordion
-    $(function(){
-    	$("#accordion").accordion({
-    		collapsible: true
-    	});
-    });
+	// Accordion
+	$(function(){
+		$("#accordion").accordion({
+			collapsible: true
+		});
+	});
 
-    // Selectable that goes inside of the accordion section Single Player
-    $("#selectableSp").selectable();
+	// Selectable that goes inside of the accordion section Single Player
+	$("#selectableSp").selectable();
 
-    // Selectable that goes insdoe of the accordion section Multiplayer
-    $("#selectableMp").selectable();
+	// Selectable that goes insdoe of the accordion section Multiplayer
+	$("#selectableMp").selectable();
 
-    // Spinner relating to hours spent gaming increasing in half hour increments
-    $("#spinner").spinner({
-    	step: 0.5,
-    	numberFormat: "n"
-    });
+	// Spinner relating to hours spent gaming increasing in half hour increments
+	$("#spinner").spinner({
+		step: 0.5,
+		numberFormat: "n"
+	});
 
-    // Submit button
-    $(".widget input[type=submit], .widget a, .widget button").button();
+	// Submit button
+	$("#subBtn").click(function(){
+		$("#progressbar").show();
+		$(function(){
+			$("#progressbar").progressbar({
+				value: 0
+			});
+		});
+	});
 
-    // Progress Bar
-    var progressbar = $("#progressbar"), 
-    progressLabel = $(".progress-label");
+	// Progress Bar
+	// First attempt -------------------
+	// var progressbar = $("#progressbar"), 
+	// progressLabel = $(".progress-label");
 
-    progressbar .progressbar({
-    	value: false,
-    	// Displays loading value by percentage on bar
-    	change: function(){
-    		progressLabel.text(progressbar .progressbar("value") + "%");
-    	},
-    	// Shows completed message on loading bar
-    	complete: function(){
-    		progressLabel.text("Complete!")
-    	}
-    });
+	// progressbar .progressbar({
+	// 	value: false,
+	// 	// Displays loading value by percentage on bar
+	// 	change: function(){
+	// 		progressLabel.text(progressbar .progressbar("value") + "%");
+	// 	},
+	// 	// Shows completed message on loading bar
+	// 	complete: function(){
+	// 		progressLabel.text("Complete!")
+	// 	}
+	// });
 
-    function progress(){
-    	var val = progressbar .progressbar("value") || 0;
-    	progressbar.progressbar("value", val + 2);
-    	if (val < 99){
-    		setTimeout(progress, 80);
-    	}
-    }
-    setTimeout(progress, 2000);
+	// function progress(){
+	// 	var val = progressbar .progressbar("value") || 0;
+	// 	progressbar.progressbar("value", val + 2);
+	// 	if (val < 99){
+	// 		setTimeout(progress, 80);
+	// 	}
+	// }
+	// setTimeout(progress, 2000);
+
+	// Second attempt ------------------
+	var pGress = setInterval(function(){
+		var pVal = $("#progressbar").progressbar("option", "value");
+		var pCnt = !isNaN(pVal) ? (pVal + 1 ) : 1;
+		if (pCnt > 100) {
+			clearInterval(pGress);
+		} else {
+			$("#progressbar").progressbar({
+				value:pCnt
+			});
+		}
+	}, 100);
 
 });
